6bb49a1c6ca281931dd31828a1d5423ee9cf7161
Anthony Mpho Matlala <74402600+EmpieichO@users.noreply.github.com>
2023-11-16 15:52:15 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-/Users/ammatlala/Library/Python/3.11/bin/virtualenv venv
+virtualenv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-mkdocs serve
+mkdocs serve --strict
 
